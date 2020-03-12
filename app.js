be0614152bd7823342ccb78695f35e764d21fa89
Matias Flores <arnod7991@gmail.com>
2020-03-12 12:41:58 -0400

    primer comiit

---
+++ app.js
@@ -0,0 +1,29 @@
+// import {crearArchivo} from './multiplicar/multiplicar';
+const {crearArchivo} = require('./multiplicar/multiplicar');
+
+var base = 6;
+crearArchivo(base).then(nombre => console.warn(nombre))
+ .catch(err => console.log(err));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let data = '';
+// for (let i = 1; i < 11; i++) {
+//   console.log(`${base} * ${i} = ${base * i}`);
+//   data += `${base} * ${i} = ${base * i} \n`; 
+// }
+
+// fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+//   if (err) throw err;
+//   console.log(`El archivo tabla-${base} ha sido creado!!!`);
+// });
